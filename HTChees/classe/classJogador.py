d053e8d73e5041003cb58189bb37455400742b88
LucasMartinsUthi <31012982+LucasMartinsUthi@users.noreply.github.com>
2018-08-07 21:51:38 -0300

    Add files via upload

--- HTChees/classe/classJogador.py
+++ HTChees/classe/classJogador.py
@@ -1,8 +1,30 @@
+from random import randint
+
 class classJogador:
 
 	def __init__(self):
-		self.deck = []
-		self.mesa = []
+		self.deck = [{'name': "queen",  'mana': 7, 'atack': 4, 'life': 6},
+                    {'name': "knight", 'mana': 4, 'atack': 4, 'life': 3},
+                    {'name': "knight", 'mana': 4, 'atack': 4, 'life': 3},
+                    {'name': "bishop", 'mana': 3, 'atack': 0, 'life': 6},
+                    {'name': "bishop", 'mana': 3, 'atack': 0, 'life': 6},
+                    {'name': "rook", 'mana': 3, 'atack': 2, 'life': 6},
+                    {'name': "rook", 'mana': 3, 'atack': 2, 'life': 6},
+                    {'name': "pawn", 'mana': 1, 'atack': 1, 'life': 6},
+                    {'name': "pawn", 'mana': 1, 'atack': 1, 'life': 6},
+                    {'name': "pawn", 'mana': 1, 'atack': 1, 'life': 6},
+                    {'name': "pawn", 'mana': 1, 'atack': 1, 'life': 6},
+                    {'name': "pawn", 'mana': 1, 'atack': 1, 'life': 6},
+                    {'name': "pawn", 'mana': 1, 'atack': 1, 'life': 6},
+                    {'name': "pawn", 'mana': 1, 'atack': 1, 'life': 6},
+                    {'name': "pawn", 'mana': 1, 'atack': 1, 'life': 6}]
+		self.mesa = {-3: {'ocupado': False},
+                   -2: {'ocupado': False}, 
+                   -1: {'ocupado': False}, 
+                    0: {'ocupado': False},
+                    1: {'ocupado': False}, 
+                    2: {'ocupado': False}, 
+                    3: {'ocupado': False}}
 		self.vida = 10
 		self.mana = 1
 		self.mao = []
@@ -11,13 +33,55 @@ class classJogador:
 		pass
 
 	def geraDeck(self):
-		pass
+		for i in reversed(range(0, len(self.deck))):
+			randI = randint(0,14)
+			randItem = self.deck[randI]
+
+			self.deck[randI] = self.deck[i]
+			self.deck[i] = randItem
 
 	def selectCarta(self):
 		pass
 
-	def addCartaMesa(self):
+	def addMana(self):
 		pass
+
+	def addMao(self):
+		self.mao.append(self.deck[0])
+		self.deck.pop(0)
+
+	def addCartaMesa(self):
+		chaves = [key for key, val in self.mesa.items() if val['ocupado']]
+
+		if not chaves:
+			mesa[0]['ocupado'] = True
+			return True
+
+		menor = min(chaves)
+		maior = max(chaves)
+
+		if abs(menor) == 3:
+			print('cheio')
+			return False
+		else:
+			if abs(menor) == abs(maior):
+				# pos2 +
+				self.mesa[pos2]['ocupado'] = True
+				i = pos2
+				while i <= maior:
+					self.mesa[i + 1] = self.mesa[i]
+					i += 1
+				return True
+			else:
+				# pos1 -
+				self.mesa[pos1]['ocupado'] = True
+				i = pos1
+				while i >= menor:
+					self.mesa[i - 1] = self.mesa[i]
+					i -= 1
+				return True
+
+		return True
 
 	def update(self):
 		pass
--- HTChees/main.py
+++ HTChees/main.py
@@ -14,27 +14,33 @@ Jogo = classJogo()
 Jogador = classJogador()
 
 
-Jogo.drawBoard()
+# Jogo.drawBoard()
 
 # teste peao
-peao = image.load('img/b_peao.png')
-peao2 = transform.smoothscale(peao, (160,220))
-peao_r = peao2.get_rect()
-
-print(peao,'\n', peao2,'\n', peao_r)
+# peao = image.load('img/b_peao.png')
+# peao2 = transform.smoothscale(peao, (160,220))
+# peao_r = peao2.get_rect()
+click = True
+for dict in Jogador.deck:
+	print(dict['name'])
 
 while True:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			sys.exit()
 
+	# Jogo.canvas.blit(peao2, peao_r)
+	carta = draw.rect(Jogo.canvas, [153, 153, 153], ([0,0],[200,200]))
+	if carta.collidepoint(pg.mouse.get_pos()):
+		carta = draw.rect(Jogo.canvas, [153, 0, 0], ([0,0],[200,200]))
 
-	Jogo.canvas.blit(peao2, peao_r)
-	pg.display.flip()
+	if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and click == True and carta.collidepoint(pg.mouse.get_pos()):
+		click = False
+		Jogador.addMao()
+	elif event.type != pygame.MOUSEBUTTONDOWN or event.button != 1 or click != False:
+		click = True
 
+	pg.display.update()
+	pg.time.Clock().tick(60)
 
-print(surface)
-
-
-
-
+# Loop com o draw do tabuleiro
